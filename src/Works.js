a7dc83fcd6efa811bda8fc7ebb86820712b6f636
Jahongir Ortiqboyev <108238210+OrtiqboyevJahongir@users.noreply.github.com>
2023-01-25 00:59:41 +0500

    ok

---
+++ src/Works.js
@@ -0,0 +1,62 @@
+
+
+const works = ( ) =>{
+    return(
+        <>
+            <main>
+                <section>
+                    <div className="container">
+                    <div className="wrapper">
+                            <h1>Featured works</h1>
+                            <div className="card">
+                                <img src="Rectangle 30.png" alt="" />
+                                <div className="card-title">
+                                    <h1>Designing Dashboards</h1>
+                                    <span className="year">2020</span> 
+                                    <span className="dash">Dashboard</span>
+                                    <p className="lorem">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+                                </div>
+                            </div>
+                    </div>  
+                    </div>                  
+                </section>
+            </main>
+
+            <main>
+                <section>
+                    <div className="container">
+                    <div className="wrapper">
+                            <div className="card">
+                                <img src="Rectangle 30.png" alt="" />
+                                <div className="card-title">
+                                    <h1>Designing Dashboards</h1>
+                                    <span className="year">{new Date().getFullYear()}</span> 
+                                    <span className="dash">Dashboard</span>
+                                    <p className="lorem">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+                                </div>
+                            </div>
+                    </div>  
+                    </div>                  
+                </section>
+            </main>
+            <main>
+                <section>
+                    <div className="container">
+                    <div className="wrapper">
+                            <div className="card">
+                                <img src="Rectangle 30.png" alt="" />
+                                <div className="card-title">
+                                    <h1>Designing Dashboards</h1>
+                                    <span className="year">2020</span> 
+                                    <span className="dash">Dashboard</span>
+                                    <p className="lorem">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+                                </div>
+                            </div>
+                    </div>  
+                    </div>                  
+                </section>
+            </main>
+        </>
+    )
+}
+export default works;
